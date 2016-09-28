422c51e556450e47e89b748d4f91192417d66156
Florian Plaza Oñate <florian.plaza@jouy.inra.fr>
2016-09-28 11:54:03 +0200

    Add missing source files

---
+++ time_profiler.cc
@@ -0,0 +1,36 @@
+/*
+ * (C) Copyright 2014-2015 Enterome
+ */
+
+#include "time_profiler.hh"
+#include <stdexcept>
+
+void TimeProfiler::start_new_timer(const std::string& timer_name)
+{
+	timers_launched_.push(std::make_pair(timer_name, std::time(NULL)));
+}
+
+
+void TimeProfiler::stop_last_timer()
+{
+	if (timers_launched_.empty())
+		throw std::runtime_error("error: no timer running.");
+
+	timer_t last_timer = timers_launched_.top();
+	timers_launched_.pop();
+	timers_finished_.push_back(
+			std::make_pair(last_timer.first, std::time(NULL)-last_timer.second));
+}
+
+std::ostream& operator<<(std::ostream& os, const TimeProfiler& tp)
+{
+    os << "Time Statistics:\n";
+
+	std::list<TimeProfiler::timer_t>::const_iterator it;
+	for (it = tp.timers_finished_.begin(); it != tp.timers_finished_.end(); it++)
+	{
+		os << it->first << ": " << it->second/60 << " min " << it->second%60 << " sec" << '\n';
+    }
+
+	return os;
+}
---
+++ time_profiler.hh
@@ -0,0 +1,28 @@
+#ifndef TIME_PROFILER_HH
+#define TIME_PROFILER_HH
+
+/*
+ * (C) Copyright 2014-2015 Enterome
+ */
+
+#include <string>
+#include <ctime>
+#include <utility>
+#include <stack>
+#include <list>
+#include <iostream>
+
+class TimeProfiler {
+    private:
+		typedef std::pair<std::string, std::time_t> timer_t;
+		std::stack<timer_t> timers_launched_;
+		std::list<timer_t> timers_finished_;
+
+    public:
+        void start_new_timer(const std::string& timer_name);
+        void stop_last_timer();
+
+        friend std::ostream& operator<<(std::ostream& os, const TimeProfiler& tp);
+};
+
+#endif // TIME_PROFILER_HH
